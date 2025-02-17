ae8aa65067536448d84615202088a0ca3b381430
Lionel Manasse <lionelmutombo.mtb@gmail.com>
2025-02-17 18:29:46 +0100

    Refactor PostgreSQL import to use package destructuring

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { Pool } from 'pg'; // PostgreSQL package
+import pkg from 'pg'; // PostgreSQL package
+const { Pool } = pkg;
 
 const app = express();
 const port = 3000;
